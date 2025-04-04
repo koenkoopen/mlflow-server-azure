0afd4a2fd4bb226242dacb33a9cb137e5abf3f0d
Koen Koopen <koen.koopen@datasciencelab.nl>
2025-04-04 12:07:41 +0200

    add providers

---
+++ terraform/providers.tf
@@ -0,0 +1,16 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.15.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+# Provider for Azure
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
